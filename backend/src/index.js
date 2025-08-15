dad6220784d7a87bde06bbd526e09f7129f7692a
Nishant Chaudhary <nishantchaudhary7557@gmail.com>
2025-08-15 23:40:06 +0530

    deployment ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,27 +3,38 @@ import router from './routes/notesRoute.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv'
 import cors from 'cors'
+import path from 'path'
 
 dotenv.config()
 
 const app = express()
-
+const __dirname = path.resolve()
 
 app.use(express.json())
-app.use(cors(
-    {
-        origin:"http://localhost:5173",
-    }
-))
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors(
+        {
+            origin: "http://localhost:5173",
+        }
+    ))
+}
 
-app.use("/api/notes",router)
+app.use("/api/notes", router)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+})
+
+}
+
 
 const port = 3001;
 
 connectDB().then(() => {
-    app.listen(port , (req, res) => {
-    console.log("Server is running on port",port);
-})
+    app.listen(port, (req, res) => {
+        console.log("Server is running on port", port);
+    })
 }
 )
 
